c634c83b08f7f1fe2f54907396b39e0bab5f72e7
MakarandPundlik <mmp2308@gmail.com>
2024-09-14 11:23:10 -0400

    Added entries

---
+++ app/views/entries/_entry.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entry, :id, :meal_type, :carbohydrates, :fats, :proteins, :calories, :created_at, :updated_at
+json.url entry_url(entry, format: :json)
---
+++ db/migrate/20240914150134_create_entries.rb
@@ -0,0 +1,16 @@
+class CreateEntries < ActiveRecord::Migration[7.2]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :carbohydrates
+      t.integer :fats
+      t.integer :proteins
+      t.integer :calories
+
+      t.timestamps
+    end
+  end
+end
+
+#add default values at this level only 
+#t.integer :retweets_count, :default => 0
